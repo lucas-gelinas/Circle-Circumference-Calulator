f1f16a77b91453ce23ea9be44cbc1026a51ab473
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-23 20:05:42 +0000

    Hopeful fix

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Connecting button and function
 document.getElementById('button').addEventListener('click', multiplication)
 
+response = 0
 diameter = 0
 
 function multiplication () {
-  diameter = document.getElementById('diameter').value
+  diameter = document.getElementById('diameter1').value
 
   diameter = parseInt(diameter)
 
